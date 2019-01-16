e8040e8dd78d13fff78d2d964cf229445e2f165e
przekwas <luke@covalence.io>
2019-01-15 22:34:06 -0600

    initial commit

---
+++ src/server/server.ts
@@ -0,0 +1,15 @@
+import * as path from 'path';
+import * as express from 'express';
+import apiRouter from './routes';
+
+const app = express();
+
+let p = path.join(__dirname, '../public');
+
+app.use(express.static(p));
+app.use(apiRouter);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});
